c3c0a8b45bc385a9832ae4dfd40a296596b6fcde
Nitin <nitin@advant.xyz>
2026-01-08 17:12:00 +0530

    i am on my way

--- hello.js
+++ hello.js
@@ -3,7 +3,8 @@ console.log("Hello");
 console.log("git");
 
 //this is very important code i am working on
-// and so on
 
+// and so on, i have to do it in less time
+// and all the formalizes 
 
-// fixing code and this is done
+// now let me focus in this 
